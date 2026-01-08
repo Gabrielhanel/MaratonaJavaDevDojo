81aa5485ebd506d3debded3299d6b4e46944b6b3
Gabriel <ghanel527@gmail.com>
2026-01-07 23:38:57 -0300

    exercicio Funcionario

---
+++ src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/FuncionarioTest01.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Funcionario;
+
+public class FuncionarioTest01 {
+    public static void main(String[] args) {
+        Funcionario funcionario = new Funcionario();
+        funcionario.nome = "Carlos";
+        funcionario.idade = 45;
+        funcionario.salarios = new double[]{5200.50, 1200, 2000};
+
+        funcionario.imprime();
+    }
+
+}
